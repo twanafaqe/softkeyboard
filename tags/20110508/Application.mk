55b64646546c38b54a04a729626cac5d02510962
mennyed@gmail.com <mennyed@gmail.com@8c604098-42ae-11de-b6cc-f32266f7ba4a>
2011-05-08 08:55:51 +0000

    Release 39 tag

---
+++ tags/20110508/Application.mk
@@ -0,0 +1,2 @@
+APP_MODULES      := anysoftkey_jni anysoftkey2_jni
+APP_PROJECT_PATH := $(call my-dir)
---
+++ tags/20110508/src/com/menny/android/anysoftkeyboard/AnyApplication.java
@@ -0,0 +1,59 @@
+package com.menny.android.anysoftkeyboard;
+
+import com.anysoftkeyboard.AnySoftKeyboardConfiguration;
+import com.anysoftkeyboard.backup.CloudBackupRequester;
+import com.anysoftkeyboard.devicespecific.DeviceSpecific;
+import com.anysoftkeyboard.devicespecific.FactoryViewBase;
+import com.anysoftkeyboard.ui.tutorials.TutorialsProvider;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+
+
+public class AnyApplication extends Application {
+
+	private static final String TAG = "ASK_APP";
+	private static AnySoftKeyboardConfiguration msConfig;
+	private static DeviceSpecific msDeviceSpecific;
+	private static CloudBackupRequester msCloudBackuper;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		if (AnySoftKeyboardConfiguration.DEBUG) Log.d(TAG, "** Starting application in DEBUG mode.");
+
+		
+		msConfig = AnySoftKeyboardConfiguration.createInstance(this);
+		
+        LayoutInflater inflate =
+            (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        FactoryViewBase factory = (FactoryViewBase)inflate.inflate(R.layout.device_specific, null);
+        msDeviceSpecific = factory.createDeviceSpecific();
+        factory = null;//GC! Please clean this view!
+        
+        msCloudBackuper = msDeviceSpecific.createCloudBackupRequester(getPackageName());
+		
+		TutorialsProvider.ShowTutorialsIfNeeded(this);
+	}
+	
+	public static AnySoftKeyboardConfiguration getConfig()
+	{
+		return msConfig;
+	}
+	
+	public static DeviceSpecific getDeviceSpecific()
+	{
+		return msDeviceSpecific;
+	}
+	
+	public static void requestBackupToCloud()
+	{
+		if (msCloudBackuper != null)
+			msCloudBackuper.notifyBackupManager();
+	}
+	
+}
